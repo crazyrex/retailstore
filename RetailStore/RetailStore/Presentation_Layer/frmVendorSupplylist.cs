081f42b7a5101fcc1f1ab91a92ff10a1a312a74f
prasengr8720 <prasengr8720@e7ee60e6-0015-11df-b136-7f7962f7bc17>
2010-01-14 11:03:56 +0000

    Created Overloading Function loadTablesJoinIntoGrid() for searching loading the Data into datagrid after filtering according to three diff textbox in Vendor_SupplyList form Few Changes in frnVendorSupplylist form textChanged event for 3 textboxes

--- RetailStore/RetailStore/Presentation_Layer/frmVendorSupplylist.cs
+++ RetailStore/RetailStore/Presentation_Layer/frmVendorSupplylist.cs
@@ -23,15 +23,21 @@ namespace RetailStore.Presentation_Layer
         }
         private void frmVendorSupplylist_Load(object sender, EventArgs e)
         {
-            d.loadTableIntoGrid("vendor_Supplylist", dgvSearchVendors);
+            d.loadTablesJoinIntoGrid("Vendor_Supplylist", "Entity", "Vendor_ID", "ID", dgvSearchVendors);
+            dgvSearchVendors.Columns["ID"].Visible = false;
+            dgvSearchVendors.Columns["Role"].Visible = false;
+            dgvSearchVendors.Columns["Email"].Visible = false;
             txtVendorId.TextChanged +=new EventHandler(txtVendorId_TextChanged);
             txtProductId.TextChanged +=new EventHandler(txtVendorName_TextChanged);
-            txtProductId.Text = vendorName;
+            txtVendorName.Text = vendorName;
+            DataTable dt = new DataTable();
+            dt = d.findRecordsLike("Entity", "Name", vendorName);
+            txtVendorId.Text=dt.Rows[0][0].ToString();
         }
 
         private void txtVendorId_TextChanged(object sender, EventArgs e)
         {
-            dgvSearchVendors.DataSource = d.findRecordsLike("Vendor_Supplylist", "Vendor_ID", txtVendorId.Text);
+            d.loadTablesJoinIntoGrid("Entity", "Vendor_Supplylist", "ID", "Vendor_ID", "ID", txtVendorId.Text.Trim(), "Name", txtVendorName.Text.Trim(), "Product_ID", txtProductId.Text.Trim(), dgvSearchVendors);
         }
 
         private void btnClose_Click(object sender, EventArgs e)
@@ -41,7 +47,13 @@ namespace RetailStore.Presentation_Layer
 
         private void txtVendorName_TextChanged(object sender, EventArgs e)
         {
-            dgvSearchVendors.DataSource = d.findRecordsLike("Vendor_Supplylist", "Product_ID", txtProductId.Text);
+            d.loadTablesJoinIntoGrid("Entity", "Vendor_Supplylist", "ID", "Vendor_ID", "ID", txtVendorId.Text.Trim(), "Name", txtVendorName.Text.Trim(), "Product_ID", txtProductId.Text.Trim(), dgvSearchVendors);
         }
+
+        private void txtProductId_TextChanged(object sender, EventArgs e)
+        {
+            d.loadTablesJoinIntoGrid("Entity", "Vendor_Supplylist", "ID", "Vendor_ID", "ID", txtVendorId.Text.Trim(), "Name", txtVendorName.Text.Trim(), "Product_ID", txtProductId.Text.Trim(), dgvSearchVendors);
+        }
+        
     }
 }
